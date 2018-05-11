c6abc480c109bf7965fe66811f6efc0acec2dbaa
galet0 <galeto.miteva@gmail.com>
2018-05-11 23:30:22 +0300

    fixed header

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,13 +1,27 @@
 
-
-var app = angular.module('app', ['ngRoute'])
+var app = angular.module('App', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
     .config(function ($routeProvider) {
         $routeProvider
             .when('/', {
                 controller: 'HomeController',
                 templateUrl: '/js/app/controllers/home/home.html'
             })
+            .when('/projections', {
+                controller: 'ProjectionsController',
+                templateUrl: '/js/app/controllers/projections/projections.html'
+            })
+            .when('/cinemas', {
+                controller: 'CinemasController',
+                templateUrl: '/js/app/controllers/projections/projections.html'
+            })
+            .otherwise({
+                controller: 'HomeController',
+                templateUrl: '/js/app/controllers/home/home.html'
+            })
     })
-
-    angular.module('app', ['ui.bootstrap']);
-    angular.module('app', ['ngAnimate']);
+    .controller('CollapseController', function ($scope) {
+        $scope.isNavCollapsed = true;
+        $scope.isCollapsed = false;
+        $scope.isCollapsedHorizontal = false;
+      });
+    
