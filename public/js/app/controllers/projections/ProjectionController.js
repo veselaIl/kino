abba12dd72fd0394edec64c3b25ef394a6900029
galet0 <galeto.miteva@gmail.com>
2018-05-27 16:06:29 +0300

    fix

--- public/js/app/controllers/projections/ProjectionController.js
+++ public/js/app/controllers/projections/ProjectionController.js
@@ -1,4 +1,4 @@
-app.controller('ProjectionController', ['$scope', '$routeParams', 'ProjectionService', 'CinemaService', function($scope, $routeParams, ProjectionService, CinemaService){
+app.controller('ProjectionController', ['$scope', '$routeParams', 'ProjectionService', function($scope, $routeParams, ProjectionService){
     
     function showProjections(date) {
         ProjectionService.getProjections(date)
@@ -17,24 +17,6 @@ app.controller('ProjectionController', ['$scope', '$routeParams', 'ProjectionSer
             });
     }
     
-    function showCinemaProjections(id, date) {
-        ProjectionService.getCinemaProjections(id, date)
-            .then(function (data) {
-                // console.log('showCinemaProjections data', data);
-                $scope.projections = Array.isArray(data.projections) ? data.projections : [];
-                $scope.cinemaDetails = data.cinema ? data.cinema : {};
-                //$scope.movieDetails = Array.isArray(data.movies) ? data.movies : [];
-                $scope.movieDetails = Array.isArray(data.movies) ? data.movies : [];
-                console.log($scope.projections, 'by ID DATE');
-                //$scope.movieProjections = Array.isArray(data.times) ? data.times : [];
-                $scope.$apply();
-            })
-            .catch(function (err){
-                console.log(err);
-            });
-    }
-
-    // $scope.projections = [];
     $scope.today = moment(new Date(),'DD-MM-YYYY');
     $scope.week = [];
     for (var i = 0; i < 7; i++){
@@ -48,20 +30,11 @@ app.controller('ProjectionController', ['$scope', '$routeParams', 'ProjectionSer
         $scope.activeDay = day;
         console.log('$scope.activeDay', $scope.activeDay);
         showProjections(day.toDate());
-        showCinemaProjections(id, day.toDate());
     }
 
     //Get all projections
     console.log('projections getProjections');
     showProjections($scope.activeDay.toDate());
-    showCinemaProjections($routeParams.id, $scope.activeDay.toDate());
-    
-    // CinemaService.getCinemas()
-    //     .then(function (cinemas){
-    //         $scope.cinemas = cinemas.cinemas;
-    //         $scope.$apply();
-    //         console.log(cinemas);
-    //     })
-    // console.log($scope);
+
     moment.locale("bg");
 }])
